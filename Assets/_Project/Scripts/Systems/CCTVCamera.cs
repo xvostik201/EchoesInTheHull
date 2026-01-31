0e7051178caabd68ac81547c50814a8f0a4a0b3a
Zakhar <zkostyutkin2004@gmail.com>
2026-01-31 13:48:38 +0500

    feat: Implement CCTV camera Light blinking

--- Assets/_Project/Scripts/Systems/CCTVCamera.cs
+++ Assets/_Project/Scripts/Systems/CCTVCamera.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Echoes.Systems
@@ -9,11 +10,26 @@ namespace Echoes.Systems
     {
         [SerializeField] private Camera _cam;
         [SerializeField] private string _cameraName = "Team module";
+        
+        [Header("Light")]
+        [SerializeField] private Light _light;
+
+        [SerializeField] private float _duration;
+        [SerializeField] private float _minIntensity;
+        [SerializeField] private float _maxIntensity;
         public string CameraName => _cameraName;
 
         private void Awake()
         {
             _cam.enabled = false;
+        }
+
+        private void Start()
+        {
+            _light.DOIntensity(_maxIntensity, _duration)
+                .From(_minIntensity) 
+                .SetEase(Ease.InOutSine)
+                .SetLoops(-1, LoopType.Yoyo);
         }
 
         public void SwitchActiveCamera(bool isActive)
